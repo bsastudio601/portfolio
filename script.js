4ff90387e952505f412a523c84f2209866973cbd
Arthi <arthihalder@gmail.com>
2026-07-08 20:00:24 +0600

    v4

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ window.addEventListener('scroll', function() {
     const scrollPercent = (scrollTop / docHeight) * 100;
     document.getElementById('progress').value = scrollPercent;
 });
+
+
+const sections = document.querySelectorAll(".page");
+
+const sectionObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, { threshold: 0.2 });
+
+sections.forEach((section) => sectionObserver.observe(section));
