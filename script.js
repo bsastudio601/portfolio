9817cfff34b23a6d9a3334330934bc5a7dbfdfd7
Arthi <arthihalder@gmail.com>
2026-07-08 17:21:26 +0600

    v3

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('scroll', function() {
+    const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+    document.getElementById('progress').value = scrollPercent;
+});
